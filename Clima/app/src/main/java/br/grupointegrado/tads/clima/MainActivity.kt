8edf55bf44ec02b278f67e479964e7ef8c83997b
Douglas Nassif Roma Junior <nassifrroma@gmail.com>
2018-04-02 20:42:57 -0300

    App Clima: Conexão com a Internet, Menus e Acabamento

--- Clima/app/src/main/java/br/grupointegrado/tads/clima/MainActivity.kt
+++ Clima/app/src/main/java/br/grupointegrado/tads/clima/MainActivity.kt
@@ -1,34 +1,89 @@
 package br.grupointegrado.tads.clima
 
+import android.os.AsyncTask
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
+import br.grupointegrado.tads.buscadorgithub.NetworkUtils
+import br.grupointegrado.tads.clima.dados.ClimaPreferencias
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-
-    val dadosClimaFicticios = listOf("Hoje - Céu limpo - 17°C / 15°C",
-            "Amanhã - Nublado - 19°C / 15°C",
-            "Quinta - Chuvoso - 30°C / 11°C",
-            "Sexta - Chuva com raios - 21°C / 9°C",
-            "Sábado - Chuva com raios - 16°C / 7°C",
-            "Domingo - Chuvoso - 16°C / 8°C",
-            "Segunda - Parcialmente nublado - 15°C / 10°C",
-            "Ter, Mai 24 - Vai curintia - 16°C / 18°C",
-            "Qua, Mai 25 - Nublado - 19°C / 15°C",
-            "Qui, Mai 26 - Tempestade - 30°C / 11°C",
-            "Sex, Mai 27 - Furacão - 21°C / 9°C",
-            "Sáb, Mai 28 - Meteóro - 16°C / 7°C",
-            "Dom, Mai 29 - Apocalipse - 16°C / 8°C",
-            "Seg, Mai 30 - Pós-apocalipse - 15°C / 10°C")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        var conteudo = ""
-        for (clima in dadosClimaFicticios) {
-            conteudo += "$clima\n\n\n"
+        carregarDadosClima()
+    }
+
+    fun carregarDadosClima() {
+        val localizacao = ClimaPreferencias.getLocalizacaoSalva(this)
+        BuscarClimaTask().execute(localizacao)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.clima, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        if (item?.itemId === R.id.acao_atualizar){
+            tv_dados_clima.text = ""
+            carregarDadosClima()
         }
-        dados_clima.text = conteudo
+        return super.onOptionsItemSelected(item)
+    }
+
+    fun exibirResultado() {
+        tv_dados_clima.visibility = View.VISIBLE
+        tv_mensagem_erro.visibility = View.INVISIBLE
+        pb_aguarde.visibility = View.INVISIBLE
+    }
+
+    fun exibirMensagemErro() {
+        tv_dados_clima.visibility = View.INVISIBLE
+        tv_mensagem_erro.visibility = View.VISIBLE
+        pb_aguarde.visibility = View.INVISIBLE
+    }
+
+    fun exibirProgressBar() {
+        tv_dados_clima.visibility = View.INVISIBLE
+        tv_mensagem_erro.visibility = View.INVISIBLE
+        pb_aguarde.visibility = View.VISIBLE
+    }
+
+    inner class BuscarClimaTask : AsyncTask<String, Void, String>() {
+
+        override fun onPreExecute() {
+            exibirProgressBar()
+        }
+
+        override fun doInBackground(vararg params: String): String? {
+            try {
+                val localizacao = params[0]
+                val url = NetworkUtils.construirUrl(localizacao)
+
+                if (url != null) {
+                    val resultado = NetworkUtils.obterRespostaDaUrlHttp(url)
+                    return resultado
+                }
+            } catch (ex: Exception) {
+                ex.printStackTrace()
+            }
+            return null
+        }
+
+        override fun onPostExecute(resultado: String?) {
+            if (resultado != null) {
+                tv_dados_clima.text = resultado
+                exibirResultado()
+            } else {
+                exibirMensagemErro()
+            }
+        }
+
     }
 }
