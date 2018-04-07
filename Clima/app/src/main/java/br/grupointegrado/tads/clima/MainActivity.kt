3e301a31d8db9c899d834fcdd8273b3124e0bc3f
Douglas Nassif Roma Junior <nassifrroma@gmail.com>
2018-04-06 21:45:46 -0300

    Solução do exercício de JSON

--- Clima/app/src/main/java/br/grupointegrado/tads/clima/MainActivity.kt
+++ Clima/app/src/main/java/br/grupointegrado/tads/clima/MainActivity.kt
@@ -3,12 +3,15 @@ package br.grupointegrado.tads.clima
 import android.os.AsyncTask
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.text.format.DateFormat
 import android.view.Menu
 import android.view.MenuItem
 import android.view.View
 import br.grupointegrado.tads.buscadorgithub.NetworkUtils
 import br.grupointegrado.tads.clima.dados.ClimaPreferencias
 import kotlinx.android.synthetic.main.activity_main.*
+import org.json.JSONObject
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -78,7 +81,33 @@ class MainActivity : AppCompatActivity() {
 
         override fun onPostExecute(resultado: String?) {
             if (resultado != null) {
-                tv_dados_clima.text = resultado
+                tv_dados_clima.text = ""
+
+                val json = JSONObject(resultado)
+                val listaPrevisoes = json.getJSONArray("list")
+
+                for (i in 0 until listaPrevisoes.length()) {
+                    val previsao = listaPrevisoes.getJSONObject(i)
+
+                    val dataLong = previsao.getLong("dt")
+                    val data = Date(dataLong * 1000)
+                    val dataFormatada = DateFormat.format("dd/MM/yyyy HH:mm", data)
+
+                    val principal = previsao.getJSONObject("main")
+                    val temperatura = principal.getDouble("temp")
+                    val umidade = principal.getDouble("humidity")
+
+                    val listaClimas = previsao.getJSONArray("weather")
+                    val clima = listaClimas.getJSONObject(0)
+                    val descricao = clima.getString("description")
+
+                    tv_dados_clima.append("Data: $dataFormatada \n")
+                    tv_dados_clima.append("Temperatura: $temperatura ºC \n")
+                    tv_dados_clima.append("Umidade: $umidade % \n")
+                    tv_dados_clima.append("Clima: $descricao \n")
+                    tv_dados_clima.append("\n\n")
+                }
+
                 exibirResultado()
             } else {
                 exibirMensagemErro()
