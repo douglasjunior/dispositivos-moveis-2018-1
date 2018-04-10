2a523d2192c63f3cc8f66f249b61ce44b0de59e1
Douglas Nassif Roma Junior <nassifrroma@gmail.com>
2018-04-09 21:10:39 -0300

    Evento de click do RecyclerView.

--- ListaVerde/app/src/main/java/br/grupointegrado/tads/listaverde/ListaVerdeAdapter.kt
+++ ListaVerde/app/src/main/java/br/grupointegrado/tads/listaverde/ListaVerdeAdapter.kt
@@ -11,16 +11,32 @@ class ListaVerdeAdapter : RecyclerView.Adapter<ListaVerdeAdapter.NumeroViewHolde
 
     val lista: List<Int>
     val context: Context
+    val itemClickListener: ItemClickListener
 
-    constructor(context: Context, lista: List<Int>) {
-        this.context = context;
+    var viewHolderCount = 0
+
+    constructor(context: Context, lista: List<Int>,
+                itemClickListener: ItemClickListener) {
+        this.context = context
         this.lista = lista
+        this.itemClickListener = itemClickListener
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NumeroViewHolder {
         val numeroListItem = LayoutInflater.from(context)
                 .inflate(R.layout.numero_list_item, parent, false);
         val numeroViewHolder = NumeroViewHolder(numeroListItem)
+
+        val tvViewHolderIndex = numeroListItem
+                .findViewById<TextView>(R.id.tv_viewholder_index)
+        tvViewHolderIndex.text = "ViewHolder $viewHolderCount"
+
+        val cor = ColorUtils
+                .getViewHolderBackgroundColor(context, viewHolderCount)
+        numeroListItem.setBackgroundColor(cor)
+
+        viewHolderCount++
+
         return numeroViewHolder
     }
 
@@ -31,17 +47,35 @@ class ListaVerdeAdapter : RecyclerView.Adapter<ListaVerdeAdapter.NumeroViewHolde
     override fun onBindViewHolder(holder: NumeroViewHolder, position: Int) {
         val numero = lista.get(position)
         holder.tvItemNumero?.text = numero.toString()
-        val cor = ColorUtils.getViewHolderBackgroundColor(context, position)
-        holder.tvItemNumero?.setBackgroundColor(cor)
     }
 
-    inner class NumeroViewHolder : RecyclerView.ViewHolder {
+    interface ItemClickListener {
+        fun onItemClick(position: Int)
+        fun onLongItemClick(position: Int)
+    }
+
+    inner class NumeroViewHolder : RecyclerView.ViewHolder,
+            View.OnClickListener, View.OnLongClickListener {
 
         val tvItemNumero: TextView?
 
         constructor(itemView: View?) : super(itemView) {
             tvItemNumero =
-                    itemView?.findViewById<TextView>(R.id.tv_item_numero);
+                    itemView?.findViewById<TextView>(R.id.tv_item_numero)
+
+            itemView?.setOnClickListener(this)
+            itemView?.setOnLongClickListener(this)
+        }
+
+        override fun onClick(p0: View?) {
+            val posicaoClicada = adapterPosition
+            itemClickListener.onItemClick(posicaoClicada)
+        }
+
+        override fun onLongClick(p0: View?): Boolean {
+            val posicaoClicada = adapterPosition
+            itemClickListener.onLongItemClick(posicaoClicada)
+            return true
         }
 
     }
--- ListaVerde/app/src/main/java/br/grupointegrado/tads/listaverde/MainActivity.kt
+++ ListaVerde/app/src/main/java/br/grupointegrado/tads/listaverde/MainActivity.kt
@@ -3,9 +3,11 @@ package br.grupointegrado.tads.listaverde
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v7.widget.LinearLayoutManager
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(),
+        ListaVerdeAdapter.ItemClickListener {
 
     val numeros = listOf(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17);
 
@@ -16,10 +18,28 @@ class MainActivity : AppCompatActivity() {
         val layoutManager = LinearLayoutManager(this,
                                     LinearLayoutManager.VERTICAL,
                                     false)
-        val adapter = ListaVerdeAdapter(this, numeros)
+        val adapter = ListaVerdeAdapter(this, numeros, this)
 
         rv_lista_numerica.layoutManager = layoutManager
         rv_lista_numerica.adapter = adapter
+    }
+
+    var toast: Toast? = null
+
+    fun exibirMensagem(mensagem: String){
+        toast?.cancel()
+        toast = Toast.makeText(this, mensagem, Toast.LENGTH_LONG)
+        toast?.show()
+    }
+
+    override fun onItemClick(position: Int) {
+        val numeroClicado = numeros.get(position)
+        exibirMensagem("Clique em $numeroClicado")
+    }
+
+    override fun onLongItemClick(position: Int) {
+        val numeroClicado = numeros.get(position)
+        exibirMensagem("Clique longo em $numeroClicado")
     }
 
 }
