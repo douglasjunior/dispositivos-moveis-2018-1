c4282055fefb5e0f05d328b488cb5010adc2f473
Douglas Nassif Roma Junior <nassifrroma@gmail.com>
2018-04-23 21:08:58 -0300

    Continuação do commit anterior.

---
+++ Clima/app/src/main/java/br/grupointegrado/tads/clima/PrevisaoAdapter.kt
@@ -0,0 +1,55 @@
+package br.grupointegrado.tads.clima
+
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+
+/**
+ * Created by dougl on 20/04/2018.
+ */
+class PrevisaoAdapter : RecyclerView.Adapter<PrevisaoAdapter.PrevisaoViewHolder> {
+
+    private var dadosClima: Array<String?>?
+
+    constructor(dadosClima: Array<String?>?) {
+        this.dadosClima = dadosClima
+    }
+
+    override fun getItemCount(): Int {
+        val dados = dadosClima
+        if (dados != null) {
+            return dados.size
+        }
+        return 0
+    }
+
+    override fun onBindViewHolder(holder: PrevisaoViewHolder, position: Int) {
+        val previsao = dadosClima?.get(position)
+        holder.tvDadosPrevisao.text = previsao
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PrevisaoViewHolder {
+        val view = LayoutInflater.from(parent.context)
+                .inflate(R.layout.previsao_list_item, parent, false)
+
+        val viewHolder = PrevisaoViewHolder(view)
+
+        return viewHolder
+    }
+
+    fun setDadosClima(dadosClima: Array<String?>?) {
+        this.dadosClima = dadosClima
+        notifyDataSetChanged()
+    }
+
+    inner class PrevisaoViewHolder : RecyclerView.ViewHolder {
+
+        val tvDadosPrevisao: TextView
+
+        constructor(itemView: View) : super(itemView) {
+            tvDadosPrevisao = itemView.findViewById(R.id.tv_dados_previsao)
+        }
+    }
+}
