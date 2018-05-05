6e3841c18fd9632c884d3a33f3df2b8c6a151646
Douglas Nassif Roma Junior <nassifrroma@gmail.com>
2018-05-04 21:00:03 -0300

    Recurso para manter informações no app CicloDeVida.

--- CicloDeVida/app/src/main/java/br/grupointegrado/tads/ciclodevida/MainActivity.kt
+++ CicloDeVida/app/src/main/java/br/grupointegrado/tads/ciclodevida/MainActivity.kt
@@ -6,14 +6,34 @@ import android.os.Bundle
 import android.util.Log
 import android.view.View
 import android.support.v4.app.ActivityCompat
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    companion object {
+        val MENSAGENS_LOG = "MENSAGENS_LOG"
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        if (savedInstanceState != null) {
+            if (savedInstanceState.containsKey(MENSAGENS_LOG)){
+                val mensagensLog = savedInstanceState.getString(MENSAGENS_LOG)
+                tv_mensagem_log.text = mensagensLog
+            }
+        }
+
         imprimir("onCreate")
+    }
+
+    override fun onSaveInstanceState(outState: Bundle?) {
+        super.onSaveInstanceState(outState)
+        imprimir("onSaveInstanceState")
+
+        val mensagensLog = tv_mensagem_log.text.toString()
+        outState?.putString(MENSAGENS_LOG, mensagensLog)
     }
 
     override fun onStart() {
@@ -48,6 +68,7 @@ class MainActivity : AppCompatActivity() {
 
     fun imprimir(mensagem: String) {
         Log.d("CicloVida", mensagem)
+        tv_mensagem_log.append("$mensagem \n")
     }
 
     /**
@@ -55,9 +76,6 @@ class MainActivity : AppCompatActivity() {
      */
     fun abrirDialog(view: View) {
         val permissoes = arrayOf(Manifest.permission.WRITE_CALENDAR)
-
-        ActivityCompat.requestPermissions(this,
-                permissoes,
-                1000)
+        ActivityCompat.requestPermissions(this, permissoes, 1000)
     }
 }
