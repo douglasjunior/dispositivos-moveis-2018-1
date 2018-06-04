8d21a8fbbd993f60e1b6b328d11038b1be5dccc6
Douglas Nassif Roma Junior <nassifrroma@gmail.com>
2018-06-04 16:43:49 -0300

    Implementado PreferenceSummary para o ListPreference no BuscadorGithub.

--- BuscadorGithub/app/src/main/java/br/grupointegrado/tads/buscadorgithub/ConfiguracaoFragment.kt
+++ BuscadorGithub/app/src/main/java/br/grupointegrado/tads/buscadorgithub/ConfiguracaoFragment.kt
@@ -1,13 +1,53 @@
 package br.grupointegrado.tads.buscadorgithub
 
+import android.content.SharedPreferences
 import android.os.Bundle
+import android.support.v7.preference.CheckBoxPreference
+import android.support.v7.preference.ListPreference
+import android.support.v7.preference.Preference
 import android.support.v7.preference.PreferenceFragmentCompat
 
-class ConfiguracaoFragment : PreferenceFragmentCompat() {
+class ConfiguracaoFragment : PreferenceFragmentCompat(),
+        SharedPreferences.OnSharedPreferenceChangeListener {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        preferenceScreen.sharedPreferences.registerOnSharedPreferenceChangeListener(this)
+    }
 
     override fun onCreatePreferences(savedInstanceState: Bundle?,
                                      rootKey: String?) {
         addPreferencesFromResource(R.xml.pref_github)
+
+        for (i in 0 until preferenceScreen.preferenceCount) {
+            val pref = preferenceScreen.getPreference(i)
+            atualizarPreferenceSummary(pref)
+        }
+    }
+
+    private fun atualizarPreferenceSummary(preference: Preference) {
+        val sharedPreferences = this.preferenceScreen.sharedPreferences
+
+        if (preference is ListPreference) {
+            val valor = sharedPreferences.getString(preference.key, "")
+            val itemSelecionado = preference.findIndexOfValue(valor)
+            if (itemSelecionado >= 0) {
+                preference.setSummary(preference.entries[itemSelecionado])
+            }
+        }
+    }
+
+    override fun onSharedPreferenceChanged(sharedPreferences: SharedPreferences?, key: String?) {
+        val pref = findPreference(key)
+        if (pref != null) {
+            atualizarPreferenceSummary(pref)
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        preferenceScreen.sharedPreferences.unregisterOnSharedPreferenceChangeListener(this)
     }
 
 }
