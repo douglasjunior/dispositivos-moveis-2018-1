b3607b90e7c5af33958b3338a0448bf1fb26b994
Douglas Nassif Roma Junior <nassifrroma@gmail.com>
2018-06-08 17:29:25 -0300

    no message

--- ListaDeEspera/app/src/main/java/br/grupointegrado/tads/listadeespera/ListaEsperaContrato.kt
+++ ListaDeEspera/app/src/main/java/br/grupointegrado/tads/listadeespera/ListaEsperaContrato.kt
@@ -1,13 +1,2 @@
 package br.grupointegrado.tads.listadeespera
 
-import android.provider.BaseColumns
-
-object ListaEsperaContrato {
-
-    object Clientes : BaseColumns {
-        const val TABELA = "clientes"
-        const val COLUNA_NOME = "nome"
-        const val COLUNA_TAMANHO_GRUPO = "tamanhoGrupo"
-        const val COLUNA_DATA_HOTA = "dataHora"
-    }
-}
