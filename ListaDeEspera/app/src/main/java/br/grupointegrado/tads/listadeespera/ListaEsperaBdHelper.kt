141278507791208e1ac0dd9f9c1abfeafa9976b8
Douglas Nassif Roma Junior <nassifrroma@gmail.com>
2018-06-11 15:29:49 -0300

    Implementado ListaEsperaContrato e ListaEsperaBdHelper

--- ListaDeEspera/app/src/main/java/br/grupointegrado/tads/listadeespera/ListaEsperaBdHelper.kt
+++ ListaDeEspera/app/src/main/java/br/grupointegrado/tads/listadeespera/ListaEsperaBdHelper.kt
@@ -7,6 +7,35 @@ import android.provider.BaseColumns
 
 class ListaEsperaBdHelper : SQLiteOpenHelper {
 
+    companion object {
+        val BD_NOME = "listaespera.db"
+        val BD_VERSAO = 1
+    }
+
+    constructor(context: Context) :
+            super(context, BD_NOME, null, BD_VERSAO) {
+    }
+
+    override fun onCreate(bd: SQLiteDatabase) {
+        val CREATE_TABLE_CLIENTES = """
+            CREATE TABLE ${ListaEsperaContrato.Clientes.TABELA} (
+                ${BaseColumns._ID} INTEGER PRIMARY KEY AUTOINCREMENT,
+                ${ListaEsperaContrato.Clientes.COLUNA_NOME} TEXT NOT NULL,
+                ${ListaEsperaContrato.Clientes.COLUNA_TAMANHO_GRUPO} INTEGER NOT NULL,
+                ${ListaEsperaContrato.Clientes.COLUNA_DATA_HORA} TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+            );
+        """
+        bd.execSQL(CREATE_TABLE_CLIENTES)
+    }
+
+    override fun onUpgrade(bd: SQLiteDatabase,
+                           versaoAnterior: Int, novaVersao: Int) {
+        bd.execSQL("DROP TABLE IF EXISTS ${ListaEsperaContrato.Clientes.TABELA};")
+        onCreate(bd)
+    }
+
 }
+
+
 
 
--- ListaDeEspera/app/src/main/java/br/grupointegrado/tads/listadeespera/ListaEsperaContrato.kt
+++ ListaDeEspera/app/src/main/java/br/grupointegrado/tads/listadeespera/ListaEsperaContrato.kt
@@ -3,5 +3,12 @@ package br.grupointegrado.tads.listadeespera
 import android.provider.BaseColumns
 
 object ListaEsperaContrato {
-    
+
+    internal object Clientes : BaseColumns {
+        const val TABELA = "clientes"
+        const val COLUNA_NOME = "nome"
+        const val COLUNA_TAMANHO_GRUPO = "tamanhoGrupo"
+        const val COLUNA_DATA_HORA = "dataHora"
+    }
+
 }
